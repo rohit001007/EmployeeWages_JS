06c34913f707df3ba076d119d8589b78f4c0f481
rohit001007 <rohit001007@gmail.com>
2020-12-27 14:46:08 +0530

    Added : Employee's Total Wage Of One Month

--- Utility.js
+++ Utility.js
@@ -1,12 +1,15 @@
 class Utility {
 
     EMP_RATE_PER_HOUR = 20
+    NUM_OF_WORKING_DAYS = 21
     IS_FULL_TIME = 1;
     IS_PART_TIME = 2;
     EMP_HR = 0;
+    DAY = 0;
     
     empCheck
     empWage
+    totalWage
 
     empAttendance() {
 
@@ -20,24 +23,28 @@ class Utility {
     }
 
     mainEmployeeWage = () => {
-        
+
         this.empAttendance();
 
-        switch(this.empCheck) {
-            
-            case this.IS_PART_TIME:
-                    this.EMP_HR = 8
-                    break;
-            
-            case this.IS_FULL_TIME:
-                    this.EMP_HR = 4    
-                    break;
+        for (this.DAY = 0; this.DAY < this.NUM_OF_WORKING_DAYS; this.DAY++) {
+
+            switch(this.empCheck) {
                 
-            default: 
-	            console.log("\n * Employee Is Absent..!!");
+                case this.IS_PART_TIME:
+                        this.EMP_HR = 8
+                        break;
+                
+                case this.IS_FULL_TIME:
+                        this.EMP_HR = 4    
+                        break;
+                    
+                default: 
+                    this.EMP_HR = 0
+            }
+            this.empDailyWage();
+            this.totalWage = this.totalWage + this.empWage;
         }
-        this.empDailyWage();
-        console.log("\n * The Daily Eamployee Wage Is : "+this.empWage);
+        console.log(`\n * The Monthly Employee Wage Is : ${this.totalWage}`);
     }
 }
 
