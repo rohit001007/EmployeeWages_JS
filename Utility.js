54c8193dfe07ad769acb9201b7fbec99f9ddca8c
rohit001007 <rohit001007@gmail.com>
2020-12-27 14:04:22 +0530

    Added : Daily Wage Of Employee

--- Utility.js
+++ Utility.js
@@ -1,19 +1,30 @@
 class Utility {
 
-    empCheck;
+    EMP_RATE_PER_HOUR = 20
+    EMP_HR = 8
+    
+    empCheck
+    empWage
 
     empAttendance() {
 
-        this.empCheck = Math.floor(Math.random() * 10) % 2;
+        this.empCheck = Math.floor(Math.random() * 10) % 3;
         return this.empCheck;
+    }
+
+    empDailyWage = () => {
+        this.empWage = this.EMP_HR * this.EMP_RATE_PER_HOUR;
+        return this.empWage;
     }
 
     mainEmployeeWage = () => {
         
         this.empAttendance();
-        
+        this.empDailyWage();
+
             if (this.empCheck == 1) {
                 console.log("\n * Employee Is Present..! ");
+                console.log("\n * The Daily Eamployee Wage Is : "+this.empWage);
             }
             else{
                 console.log("\n * Employee Is Absent..! ");
